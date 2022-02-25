2e11ab732c17c36b8619e4ea069e48b6fc2bd4f0
Arai-kun <koki.alright@gmail.com>
2022-02-26 00:15:47 +0900

    c

--- client/src/app/db.service.ts
+++ client/src/app/db.service.ts
@@ -34,8 +34,16 @@ export class DbService {
 
   add<T>(kind: string, data: T): Observable<boolean> {
     const url = `db/${kind}`;
-    return this.http.post<boolean>(url, data, this.httpOptions)
+    return this.http.post<T>(url, data, this.httpOptions)
     .pipe(
+      map(result => {
+        if(result){
+          return true;
+        }
+        else{
+          return false;
+        }
+      }),
       catchError(this.handleError<boolean>(false))
     );
   }
