452265fff45f34f292e82ca5601c127805ad910f
Arai-kun <koki.alright@gmail.com>
2022-02-25 12:01:39 +0900

    a

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DmtoolComponent } from './dmtool/dmtool.component';
 import { LogComponent } from './log/log.component';
 import { AuthComponent } from './auth/auth.component';
+import { OauthComponent } from './oauth/oauth.component';
 
 const routes: Routes = [
   { path: 'dmtool', component: DmtoolComponent },
   { path: 'dmtool/auth', component: AuthComponent },
-  { path: 'log', component: LogComponent }
+  { path: 'log', component: LogComponent },
+  { path: 'oauth', component: OauthComponent },
+  
 ];
 
 @NgModule({
