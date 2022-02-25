920db297dc126e887c28ea740fbf3dca12852442
Arai-kun <koki.alright@gmail.com>
2022-02-25 19:17:05 +0900

    c

--- server/task.js
+++ server/task.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
-let Twitter = require('../models/twitter');
+let Twitter = require('./models/twitter');
 const { TwitterClient } = require('twitter-api-client');
 require('dotenv').config();
 
