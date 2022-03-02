9416e7e9c73d9f92d32cb5afa1473cb96916892a
Arai-kun <koki.alright@gmail.com>
2022-03-02 16:49:03 +0900

    c

--- server/models/log.js
+++ server/models/log.js
@@ -9,5 +9,5 @@ let logSchema = new Schema({
     event: Number,
 });
 
-logSchema.plugin(autoIncrement, {inc_field: 'no', reference_field: ['email']});
+logSchema.plugin(autoIncrement, {inc_field: 'no', reference_fields: ['email']});
 module.exports = mongoose.model("Log", logSchema, 'log');
