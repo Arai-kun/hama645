c6ccbbceb27cfa0777b3c17bf3949f6e4ebf91ad
Arai-kun <koki.alright@gmail.com>
2022-03-06 22:24:29 +0900

    c

--- server/routes/ws.js
+++ server/routes/ws.js
@@ -15,55 +15,10 @@ const userActivityWebhook = twitterWebhooks.userActivity({
   //app: app
 });
 //userActivityWebhook.register();
-async () => {
-let tw = {
-	user_id: '',
-	oauth_token: '',
-	oauth_token_secret: ''
-}
-try {
-	let twitter = await Twitter.findOne({email: req.user['email'], screen_name: req.params.id}).exec();
-	tw.user_id = twitter.user_id;
-	tw.oauth_token = twitter.oauth_token;
-	tw.oauth_token_secret = twitter.oauth_token_secret;
-}
-catch(error) {
-	next(error);
-}
-}
-
-
-userActivityWebhook.subscribe({
-	userId: tw.user_id,
-	accessToken: tw.oauth_token,
-	accessTokenSecret: tw.oauth_token_secret
-})
-.then(userActivity => {
-	userActivity
-	//.on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
-	//.on ('tweet_create', (data) => console.log (userActivity.id + ' - tweet_create'))
-	.on('follow', (data) => {
-		console.log(data);
-		ws.send({text: 'Receive msg', date: 'date'});
-	})
-	//.on ('mute', (data) => console.log (userActivity.id + ' - mute'))
-	//.on ('revoke', (data) => console.log (userActivity.id + ' - revoke'))
-	.on('direct_message', data => {
-		console.log(data);
-		ws.send({text: 'Receive msg', date: 'date'});
-	})
-	.on('direct_message_indicate_typing', (data) => {
-		console.log(data);
-		ws.send({text: 'Typing', date: 'date'});
-	})
-	//.on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
-	//.on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
-});
 
 router.ws('/:id', async (ws, req) => {
 	/* Connected */
 	console.log('connected');
-	/*
 	let tw = {
 		user_id: '',
 		oauth_token: '',
@@ -80,11 +35,11 @@ router.ws('/:id', async (ws, req) => {
 	}
 
 	
-	userActivityWebhook.subscribe({
+	let userActivity = await userActivityWebhook.subscribe({
 		userId: tw.user_id,
 		accessToken: tw.oauth_token,
 		accessTokenSecret: tw.oauth_token_secret
-	})
+	})/*
 	.then(userActivity => {
 		userActivity
 		//.on('favorite', (data) => console.log (userActivity.id + ' - favorite'))
@@ -106,6 +61,11 @@ router.ws('/:id', async (ws, req) => {
 		//.on ('direct_message_mark_read', (data) => console.log (userActivity.id + ' - direct_message_mark_read'))
 		//.on ('tweet_delete', (data) => console.log (userActivity.id + ' - tweet_delete'))
 	});*/
+
+	userActivity.on('direct_message', data => {
+		console.log(data);
+		ws.send({text: 'Receive msg', date: 'date'});
+	});
 
 	ws.on('message', (msg) => {
 		console.log('message');
